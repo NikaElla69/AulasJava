0819741afe3b53e08fa620ff6d72073f648ce91c
Camilla Gomes <camilla.gomeseugenio2005@gmail.com>
2024-11-26 22:23:21 -0300

    tabela clientes

---
+++ src/main/java/br/grupointegrado/lanches/model/Cliente.java
@@ -0,0 +1,53 @@
+package br.grupointegrado.lanches.model;
+
+import jakarta.persistence.*;
+
+@Entity //faz o spring entender como uma classe que trabalha na biblioteca de persistencia
+@Table(name = "clientes") //representa uma tabela
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String email;
+
+    @Column
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
